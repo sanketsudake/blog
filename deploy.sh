d682547539e8b2007ada11a1b23ca9a7ac305468
Sanket Sudake <sanketsudake@gmail.com>
2026-03-11 09:41:38 +0530

    Add github action for sync
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo
 cd public
+
+## Ensure we have CNAME file for custom domain
+# ssudake.com
+# www.ssudake.com
+echo "ssudake.com" > CNAME
+echo "www.ssudake.com" >> CNAME
+
+## Commit and push changes to GitHub
 git add .
 msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
